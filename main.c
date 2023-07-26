6c753924a1893667a71629863b0c90e204d789a1
DEVANSU1998 <122101435+DEVANSU1998@users.noreply.github.com>
2023-07-26 11:27:34 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,32 @@
+
+#define  add(intx,inty)  (sum=a+b)
+#define E_OK  10
+
+int sum;
+
+
+int main()
+{
+  int a,b;
+  printf("ENTER VALUE A");
+  scanf("%d",&a);
+  printf("ENTER VALUE B");
+  scanf("%d",&b);
+
+
+
+    int result=add(a,b);
+    printf("%d\n",result);
+
+    if(sum==10)
+{
+    printf("%d\n",E_OK);
+
+}
+return 0;
+
+}
+
+
+
+
